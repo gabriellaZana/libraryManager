259d391d7af8d1e6e8e737d50fbbfc4d8be2c8aa
gabriellaZana <zanagabriella@gmail.com>
2018-01-30 14:10:56 +0100

    basics of book class

---
+++ src/java/com/threelittlepigs/codecool/libraryManager/Entities/Book.java
@@ -0,0 +1,111 @@
+package java.com.threelittlepigs.codecool.libraryManager.Entities;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String title;
+    private String author;
+
+    @Column(name = "picture")
+    private String picture_url;
+
+    private int year;
+    private String description;
+    private String publisher;
+    private Genre genre;
+    private Location location;
+    private String isbn;
+    private boolean isAvailable = true;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPicture_url() {
+        return picture_url;
+    }
+
+    public void setPicture_url(String picture_url) {
+        this.picture_url = picture_url;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
