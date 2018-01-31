3cdd803fa9f42211057c4c4590385410b76b7785
György Mátyás <gy.matyas97@gmail.com>
2018-01-31 11:55:19 +0100

    entitymanager is always open

--- src/java/com/threelittlepigs/codecool/libraryManager/Utils/EntityUtility.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Utils/EntityUtility.java
@@ -12,48 +12,37 @@ public class EntityUtility {
 
     private static final Logger logger = LoggerFactory.getLogger(EntityUtility.class);
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("libraryPU");
-
-    private static EntityManager entityManager;
+    private static final EntityManager entityManager = entityManagerFactory.createEntityManager();
     private static EntityTransaction entityTransaction;
 
     public static void persistEntity(Object object) {
         logger.info("Persisting object : " + object.getClass().getSimpleName());
-        createTransaction();
 
+        createTransaction();
         entityTransaction.begin();
         entityManager.persist(object);
         entityTransaction.commit();
-
-        closeTransaction();
     }
 
     public static void mergeEntity(Object object) {
         logger.info("Merging object : " + object.getClass().getSimpleName());
-        createTransaction();
 
+        createTransaction();
         entityTransaction.begin();
         entityManager.merge(object);
         entityTransaction.commit();
-
-        closeTransaction();
     }
 
     public static void removeEntity(Object object) {
         logger.info("Removing object : " + object.getClass().getSimpleName());
-        createTransaction();
 
+        createTransaction();
         entityTransaction.begin();
         entityManager.remove(object);
         entityTransaction.commit();
-        closeTransaction();
-    }
-
-    private static void closeTransaction() {
-        entityManager.close();
     }
 
     private static void createTransaction() {
-        entityManager = entityManagerFactory.createEntityManager();
         entityTransaction = entityManager.getTransaction();
     }
 
