9c793c05f0b9ea2dc830234f8f87de45c16db0c5
György Mátyás <gy.matyas97@gmail.com>
2018-02-13 14:38:28 +0100

    singleton for entityUtility

--- src/java/com/threelittlepigs/codecool/libraryManager/Utils/EntityUtility.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Utils/EntityUtility.java
@@ -8,20 +8,30 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.Map;
 
 public class EntityUtility {
+
+    private static EntityUtility instance = null;
+
     private static final Logger logger = LoggerFactory.getLogger(EntityUtility.class);
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("libraryPU");
+
+
+    private static final EntityManager entityManager = entityManagerFactory.createEntityManager();
+    private static EntityTransaction entityTransaction;
 
     public static EntityManager getEntityManager() {
         return entityManager;
     }
+    public static EntityUtility getInstance() {
+        if (instance == null) {
+            instance = new EntityUtility();
+        }
+        return instance;
+    }
 
-    private static final EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-    private static EntityTransaction entityTransaction;
-
-    public static void persistEntity(Object object) {
+    public void persistEntity(Object object) {
         logger.info("Persisting object : " + object.getClass().getSimpleName());
 
         createTransaction();
@@ -30,7 +40,7 @@ public class EntityUtility {
         entityTransaction.commit();
     }
 
-    public static void mergeEntity(Object object) {
+    public void mergeEntity(Object object) {
         logger.info("Merging object : " + object.getClass().getSimpleName());
 
         createTransaction();
@@ -39,7 +49,7 @@ public class EntityUtility {
         entityTransaction.commit();
     }
 
-    public static void removeEntity(Object object) {
+    public void removeEntity(Object object) {
         logger.info("Removing object : " + object.getClass().getSimpleName());
 
         createTransaction();
@@ -48,11 +58,11 @@ public class EntityUtility {
         entityTransaction.commit();
     }
 
-    private static void createTransaction() {
+    private void createTransaction() {
         entityTransaction = entityManager.getTransaction();
     }
 
-    public static List likeByOneCriteria(Class c, String columnTitle, String columnValue ) {
+    public List likeByOneCriteria(Class c, String columnTitle, String columnValue) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery cq = cb.createQuery(c);
         Root bk = cq.from(c);
@@ -62,7 +72,7 @@ public class EntityUtility {
         return query.getResultList();
     }
 
-    public static List findByOneCriteria(Class c, String columnTitle, String columnValue ) {
+    public List findByOneCriteria(Class c, String columnTitle, String columnValue) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery cq = cb.createQuery(c);
         Root bk = cq.from(c);
@@ -70,5 +80,15 @@ public class EntityUtility {
                 .where(cb.equal(bk.get(columnTitle), columnValue));
         TypedQuery query = entityManager.createQuery(cq);
         return query.getResultList();
+    }
+
+    public Object findByTwoCriteria(Class c, List<String> columnTitles, List<String> columnValues) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery cq = cb.createQuery(c);
+        Root bk = cq.from(c);
+        cq.select(bk)
+                .where(cb.and(cb.equal(bk.get(columnTitles.get(0)), columnTitles.get(0)),cb.equal(bk.get(columnTitles.get(1)), columnValues.get(1))));
+        TypedQuery query = entityManager.createQuery(cq);
+        return query.getSingleResult();
     }
 }
