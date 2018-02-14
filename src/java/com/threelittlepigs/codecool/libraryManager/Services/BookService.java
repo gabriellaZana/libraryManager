a2cacf40dd2035e9e6a26434a4c8bd8776cf632f
gabriellaZana <zanagabriella@gmail.com>
2018-02-14 09:49:12 +0100

    some methods done

--- src/java/com/threelittlepigs/codecool/libraryManager/Services/BookService.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Services/BookService.java
@@ -6,10 +6,10 @@ import com.threelittlepigs.codecool.libraryManager.Entities.Book;
 public interface BookService {
     Book getBookById(int id);
     Book getBookByTitle(String title);
-    void changeTitle();
-    void changeAuthor();
+    void changeTitle(String title, int id);
+    void changeAuthor(String author, int id);
     void changePicture();
-    void changeYear();
+    void changeYear(int year, int id);
     void changeDescription();
     void changePublisher();
     void changeGenre();
--- src/java/com/threelittlepigs/codecool/libraryManager/Services/Implementations/BookServiceJPA.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Services/Implementations/BookServiceJPA.java
@@ -2,31 +2,42 @@ package com.threelittlepigs.codecool.libraryManager.Services.Implementations;
 
 import com.threelittlepigs.codecool.libraryManager.Entities.Book;
 import com.threelittlepigs.codecool.libraryManager.Services.BookService;
+import com.threelittlepigs.codecool.libraryManager.Utils.EntityUtility;
+
+import java.util.List;
 
 
 public class BookServiceJPA implements BookService {
     public Book getBookById(int id) {
-        return null;
+        List<Book> books = EntityUtility.findByOneCriteria(Book.class, "id", String.valueOf(id));
+        return books.get(0);
     }
 
     public Book getBookByTitle(String title) {
-        return null;
+        List<Book> books = EntityUtility.findByOneCriteria(Book.class, "title", title);
+        return books.get(0);
     }
 
-    public void changeTitle() {
-
+    public void changeTitle(String title, int id) {
+        Book book = getBookById(id);
+        book.setTitle(title);
+        EntityUtility.mergeEntity(book);
     }
 
-    public void changeAuthor() {
-
+    public void changeAuthor(String author, int id) {
+        Book book = getBookById(id);
+        book.setAuthor(author);
+        EntityUtility.mergeEntity(book);
     }
 
     public void changePicture() {
 
     }
 
-    public void changeYear() {
-
+    public void changeYear(int year, int id) {
+        Book book = getBookById(id);
+        book.setYear(year);
+        EntityUtility.mergeEntity(book);
     }
 
     public void changeDescription() {
@@ -50,6 +61,6 @@ public class BookServiceJPA implements BookService {
     }
 
     public void changeAvailability() {
-        
+
     }
 }
