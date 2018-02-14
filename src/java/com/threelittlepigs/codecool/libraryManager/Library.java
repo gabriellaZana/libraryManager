a204eb6b3778db2d71992e71aefee453a93f2bde
gabriellaZana <zanagabriella@gmail.com>
2018-02-14 10:48:49 +0100

    filing:)

--- src/java/com/threelittlepigs/codecool/libraryManager/Library.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Library.java
@@ -1,6 +1,13 @@
 package com.threelittlepigs.codecool.libraryManager;
 
+import com.threelittlepigs.codecool.libraryManager.Entities.Book;
+import com.threelittlepigs.codecool.libraryManager.Entities.Fine;
+import com.threelittlepigs.codecool.libraryManager.Entities.Users.Librarian;
+import com.threelittlepigs.codecool.libraryManager.Entities.Users.Member;
+import com.threelittlepigs.codecool.libraryManager.Enums.Genre;
+import com.threelittlepigs.codecool.libraryManager.Enums.Location;
 import com.threelittlepigs.codecool.libraryManager.Utils.Controller;
+import com.threelittlepigs.codecool.libraryManager.Utils.EntityUtility;
 import com.threelittlepigs.codecool.libraryManager.Utils.ThymleafBookController;
 import spark.Request;
 import spark.Response;
@@ -25,7 +32,7 @@ public class Library {
 
         get("/books/:isbn", (Request req, Response res) -> {
             String isbn = req.params(":isbn");
-            return new ThymeleafTemplateEngine().render(controller.renderBook(req, res, "book",isbn));
+            return new ThymeleafTemplateEngine().render(controller.renderBook(req, res, "book", isbn));
         });
         //populateDB();
         enableDebugScreen();
@@ -55,6 +62,7 @@ public class Library {
         List books1 = EntityUtility.findByOneCriteria(Book.class, "title", "Kis 1");
         System.out.println(books1);
         EntityUtility.mergeEntity(book);
+
     }*/
     }
 }
