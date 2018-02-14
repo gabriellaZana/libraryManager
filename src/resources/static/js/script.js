6f23d69b655cd8cbeba7d6c2d6443ef635fc80b5
gabriellaZana <zanagabriella@gmail.com>
2018-02-14 16:03:54 +0100

    register function added in js

--- src/resources/static/js/script.js
+++ src/resources/static/js/script.js
@@ -16,7 +16,53 @@ function checkPass() {
     }
 }
 
+function register(){
+    $('#register').on('submit', function (event) {
+        event.preventDefault();
+        var userData = new Map();
+
+        userData.set("username", $("#username").val());
+        userData.set("firstname", $("#firstname").val());
+        userData.set("lastname", $("#lastname").val());
+        userData.set("address", $("#address").val());
+        userData.set("email", $("#newemail").val());
+        userData.set("phonenum", $("#phonenum").val());
+        userData.set("dateofbirth", $("#dateOfBirth").val());
+        userData.set("password", $("#newpwd").val());
+
+        $.ajax({
+            url:'/register',
+            type: 'POST',
+            contentType: 'application/json; charset=UTF-8',
+            data:JSON.stringify(userData),
+            success: function (response) {
+                if(response == '"failure"'){
+                    alert("Email is already in use!");
+                    $("#newpwd").val("");
+                    $("#newpwd2").val("");
+                    $("#newemail").val("");
+                } else {
+                    $("#myLoginModal").hide();
+                    alert("Thank you for registering!");
+                    $(function () {
+                        $('#myLoginModal').modal('toggle');
+                    });
+                    $("#username").val("");
+                    $("#firstname").val("");
+                    $("#lastname").val("");
+                    $("#address").val("");
+                    $("#newemail").val("");
+                    $("#phonenum").val("");
+                    $("#dateOfBirth").val("");
+                    $("#newpwd").val("");
+                    $("#newpwd2").val("");
+                }
+            }
+        })
+    })
+}
+
 
 $(document).ready(function () {
-    alert("hahó")
+    register();
 });
