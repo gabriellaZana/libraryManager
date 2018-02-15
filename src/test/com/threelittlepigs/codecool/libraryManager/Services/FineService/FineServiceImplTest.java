56fb1079a29e4a45222f79f41e627a6001a8121a
mateBedok <mate.bedok@gmail.com>
2018-02-15 12:47:10 +0100

    Fines by user id test implemented

--- src/test/com/threelittlepigs/codecool/libraryManager/Services/FineService/FineServiceImplTest.java
+++ src/test/com/threelittlepigs/codecool/libraryManager/Services/FineService/FineServiceImplTest.java
@@ -2,11 +2,13 @@ package com.threelittlepigs.codecool.libraryManager.Services.FineService;
 
 import com.threelittlepigs.codecool.libraryManager.Entities.Fine;
 import com.threelittlepigs.codecool.libraryManager.Entities.Users.Member;
+import com.threelittlepigs.codecool.libraryManager.Utils.EntityUtility;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -16,21 +18,22 @@ class FineServiceImplTest {
 
     private FineService fineService;
     private Fine fine;
+    private Member member;
 
     @BeforeAll
     void init() {
         fineService = new FineServiceImpl();
         fine = fineService.getFineById(1);
+        member = mock(Member.class);
+        when(member.getId()).thenReturn(2L);
     }
 
     @Test
     void changeAmountTest() {
 
         Double formerAmount = fine.getAmount();
-        //System.out.println(formerAmount);
         fineService.changeAmount((int) fine.getId(), formerAmount + 5);
         Double presentAmount = fine.getAmount();
-        //System.out.println(presentAmount);
         assertNotEquals(formerAmount, presentAmount);
     }
 
@@ -39,8 +42,6 @@ class FineServiceImplTest {
         Date formerDate = fine.getOccurrence();
         Date presentDate = new Date();
         fineService.changeOccurrence((int) fine.getId(), presentDate);
-        System.out.println(formerDate);
-        System.out.println(presentDate);
         assertNotEquals(formerDate, presentDate);
 
     }
@@ -50,8 +51,6 @@ class FineServiceImplTest {
         Date formerDate = fine.getDueDate();
         Date presentDate = new Date();
         fineService.changeDueDate((int) fine.getId(), presentDate);
-        System.out.println(formerDate);
-        System.out.println(presentDate);
         assertNotEquals(formerDate, presentDate);
     }
 
@@ -59,26 +58,19 @@ class FineServiceImplTest {
     void changeStatusTest() {
         Boolean formerStatus = fine.isStatus();
         fineService.changeStatus((int) fine.getId(), !formerStatus);
-        System.out.println(formerStatus);
-        System.out.println(fine.isStatus());
         assertNotEquals(formerStatus, fine.isStatus());
     }
 
     @Test
     void changeMemberTest() {
-        Member member = mock(Member.class);
-        when(member.getId()).thenReturn(2L);
         fineService.changeMember((int) fine.getId(), (int)member.getId());
         assertEquals(member.getId(), fine.getMember().getId());
     }
 
-    // TODO
     @Test
     void getFinesByUserIdTest() {
-        /*Member member = mock(Member.class);
-        when(member.getId()).thenReturn(2L);
-        List<Fine> userFines = EntityUtility.findByOneCriteria(Fine.class, "member", "2");
-        assertEquals(1, userFines.size());*/
+        List<Fine> userFines = EntityUtility.getEntityManager().createNamedQuery("getFineByMember", Fine.class).setParameter("memberid", member.getId()).getResultList();
+        assertEquals(2, userFines.size());
     }
 
     @Test
