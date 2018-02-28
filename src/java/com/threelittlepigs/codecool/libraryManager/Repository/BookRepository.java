5b32cceca07c6dc7c9e4fab9931d0f4b4976ad86
György Mátyás <gy.matyas97@gmail.com>
2018-02-28 11:09:23 +0100

    fixed merge

--- src/java/com/threelittlepigs/codecool/libraryManager/Repository/BookRepository.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Repository/BookRepository.java
@@ -16,7 +16,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> getAllByRentedByMemberOrderByIsbn(User rentedById);
     List<Book> getBooksByTitleOrderByIsbn(String title);
     Book getBookByIsbnOrderByIsbn(String isbn);
-    List<Book> getBooksByTitle(String title);
-    Book getBookByIsbn(String isbn);
 
 }
