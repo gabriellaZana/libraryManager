bee9de88412f4356535461021321b3d8d52e9fd9
Unknown <surfinbird245@gmail.com>
2018-02-28 14:53:29 +0100

    Search working

--- src/java/com/threelittlepigs/codecool/libraryManager/Controllers/Implementations/SessionController.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Controllers/Implementations/SessionController.java
@@ -2,6 +2,7 @@ package com.threelittlepigs.codecool.libraryManager.Controllers.Implementations;
 
 import com.threelittlepigs.codecool.libraryManager.Entities.Book;
 import com.threelittlepigs.codecool.libraryManager.Entities.Fine;
+import com.threelittlepigs.codecool.libraryManager.Entities.Users.Librarian;
 import com.threelittlepigs.codecool.libraryManager.Entities.Users.Member;
 import com.threelittlepigs.codecool.libraryManager.Entities.Users.User;
 import com.threelittlepigs.codecool.libraryManager.Services.BookService;
@@ -15,7 +16,10 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,7 +45,30 @@ public class SessionController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String renderBooks(Model model) {
         List<Book> books = bookService.findAll();
-        model.addAttribute("books", books);
+        List<Book> booksToRender = new ArrayList<>();
+        String title = null;
+        for(Book book : books){
+            if (book.getTitle().equals(title)) continue;
+            title = book.getTitle();
+            booksToRender.add(book);
+        }
+        model.addAttribute("books", booksToRender);
+        model.addAttribute("user_id", currentUser != null ? currentUser.getId() : 0 );
+        model.addAttribute("userName", currentUser != null ? currentUser.getUserName() : "");
+        return "index";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String renderSearch(HttpServletRequest request, Model model){
+        List<Book> books = bookService.getBooksByTitleOrAuthorIsContaining(request.getParameter("search"));
+        List<Book> booksToRender = new ArrayList<>();
+        String title = null;
+        for(Book book : books){
+            if (book.getTitle().equals(title)) continue;
+            title = book.getTitle();
+            booksToRender.add(book);
+        }
+        model.addAttribute("books", booksToRender);
         model.addAttribute("user_id", currentUser != null ? currentUser.getId() : 0 );
         model.addAttribute("userName", currentUser != null ? currentUser.getUserName() : "");
         return "index";
--- src/java/com/threelittlepigs/codecool/libraryManager/Repository/BookRepository.java
+++ src/java/com/threelittlepigs/codecool/libraryManager/Repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.threelittlepigs.codecool.libraryManager.Repository;
 import com.threelittlepigs.codecool.libraryManager.Entities.Book;
 import com.threelittlepigs.codecool.libraryManager.Entities.Users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +16,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> getAllByReservedByMemberOrderByIsbn(User reservedById);
     List<Book> getAllByRentedByMemberOrderByIsbn(User rentedById);
     List<Book> getBooksByTitleOrderByIsbn(String title);
+    List<Book> getBooksByTitleIsContainingOrAuthorIsContaining(String title, String author);
+
     Book getBookByIsbnOrderByIsbn(String isbn);
 
 }
